302df690525ba594c48d05af49e92e18cc02519e
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-20 16:47:35 -0700

    Initial API tests

--- apis/tests.py
+++ apis/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase  # noqa:F401
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from books.models import Book
+
+
+class APITests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title="Django for APIs",
+            subtitle="Build web APIs with Python and Django",
+            author="William S. Vincent",
+            pages="200",
+            publisher="Lean Publishing",
+            pubdate="2022-04-01",
+            price="29.99",
+            isbn="9781735467221",
+        )
+
+    def test_api_listview(self):
+        response = self.client.get(reverse("book_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Book.objects.count(), 1)
+        self.assertContains(response, self.book)
