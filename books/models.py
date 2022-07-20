02303635570a64df41d8afb596aa472e7770c8b5
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-19 18:38:28 -0700

    Create book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
-from django.db import models  # noqa:F401
+from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=250)
+    author = models.CharField(max_length=100)
+    pages = models.IntegerField(help_text="Number of Pages")
+    publisher = models.CharField(max_length=40)
+    pubdate = models.DateField(help_text="Date Published")
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.title
