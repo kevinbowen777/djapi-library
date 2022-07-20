2c464bc91ec83958157ee240610a53f04f209163
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-20 12:02:51 -0700

    Make subtitle optional

--- books/models.py
+++ books/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=250)
-    subtitle = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=250, blank=True)
     author = models.CharField(max_length=100)
     pages = models.IntegerField(help_text="Number of Pages")
     publisher = models.CharField(max_length=40)
