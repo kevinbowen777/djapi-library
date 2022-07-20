3f68bac6b5981d22af621703e3c7bf0f711a4a06
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-20 14:16:43 -0700

    Initial books app tests

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase  # noqa:F401
+from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Book
+
+
+class BookTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title="Code Complete",
+            subtitle="A handbook of software construction",
+            author="Steve McConnell",
+            pages="952",
+            publisher="Microsoft Press",
+            pubdate="2004-03-29",
+            price="65.72",
+            isbn="0735619670",
+        )
+
+    def test_book_content(self):
+        self.assertEqual(self.book.title, "Code Complete")
+        self.assertEqual(
+            self.book.subtitle, "A handbook of software construction"
+        )
+        self.assertEqual(self.book.author, "Steve McConnell")
+        self.assertEqual(self.book.pages, "952")
+        self.assertEqual(self.book.publisher, "Microsoft Press")
+        self.assertEqual(self.book.pubdate, "2004-03-29")
+        self.assertEqual(self.book.price, "65.72")
+        self.assertEqual(self.book.isbn, "0735619670")
+
+    def test_book_listview(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "A handbook of software construction")
+        self.assertTemplateUsed(response, "books/book_list.html")
