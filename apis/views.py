8718f51492965cdf8e15334fcc051c01e9f972f1
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-29 20:25:47 -0700

    Add IsAuthorOrReadOnly custom permissions

--- apis/views.py
+++ apis/views.py
@@ -1,14 +1,17 @@
 from rest_framework import generics
 
 from books.models import Book
+from .permissions import IsAuthorOrReadOnly
 from .serializers import BookSerializer
 
 
 class BookList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
 class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Book.objects.all()
     serializer_class = BookSerializer
