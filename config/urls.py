c34a373f8837ef56bd6dcd6862681c4f6d8e4adc
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-29 19:13:56 -0700

    Add versioning to API URL

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("apis.urls")),
+    path("api/v1/", include("apis.urls")),
     path("", include("books.urls")),
 ]
