9873350e68a17fa7997d0da8987baa160e1426c9
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-30 13:08:15 -0700

    Comment out admin

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import include, path
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
     path("api/v1/", include("apis.urls")),
     path("api-auth/", include("rest_framework.urls")),
     path("", include("books.urls")),
