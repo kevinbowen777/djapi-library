56d15ae9dabfdb0a3866c2021802f354926155ca
Kevin Bowen <kevin.bowen@gmail.com>
2022-08-01 14:47:23 -0700

    Wire User views to URL route

--- apis/urls.py
+++ apis/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import BookList, BookDetail
+from .views import BookList, BookDetail, UserList, UserDetail
 
 
 urlpatterns = [
+    path("users/", UserList.as_view()),
+    path("users/<int:pk>/", UserDetail.as_view()),
     path("", BookList.as_view(), name="book_list"),
     path("<int:pk>/", BookDetail.as_view(), name="book_detail"),
 ]
