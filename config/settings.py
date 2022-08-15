6d5ab62da2e71627b52b6bd726c901b57f078568
Kevin Bowen <kevin.bowen@gmail.com>
2022-08-14 22:38:04 -0700

    Add pagination. Resolves #34

--- config/settings.py
+++ config/settings.py
@@ -144,6 +144,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 3,
 }
 
 CORS_ORIGIN_WHITELIST = (
