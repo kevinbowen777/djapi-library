ba3dd2b6394d921f05c01a251b628a85effd02bb
Kevin Bowen <kevin.bowen@gmail.com>
2023-01-06 16:19:00 -0800

    Create email contact page. Resolves #139

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,18 @@
 from books.views import BookListView
 from django.urls import path
 
-from .views import AboutPageView, HomePageView
+from .views import (
+    AboutPageView,
+    ContactView,
+    HomePageView,
+    SuccessView,
+)
 
 
 urlpatterns = [
     path("about/", AboutPageView.as_view(), name="about"),
+    path("contact/", ContactView, name="contact"),
+    path("success/", SuccessView, name="success"),
     path("", HomePageView.as_view(), name="home"),
     path("books/", BookListView.as_view(), name="booklist"),
 ]
