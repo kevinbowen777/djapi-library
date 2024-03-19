61c2cfaf7969445df741f006accf7e59664997b0
Kevin Bowen <kevin.bowen@gmail.com>
2024-03-18 19:18:51 -0700

    Add vulnerability exclusion id 65213. Resolves #371 (#372)

--- noxfile.py
+++ noxfile.py
@@ -100,6 +100,8 @@ def safety(session):
             "safety",
             "check",
             f"--file={requirements.name}",
+            "--ignore",
+            "65213",
             "--full-report",
         )
 
